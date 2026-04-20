fc6e37669638c7ac99aa0352b9c67ff69c30184e
martin <martin.ferk1@student.um.si>
2026-04-20 13:10:52 +0200

    Popravki v creategruop formu

--- frontend/src/createGroup.jsx
+++ frontend/src/createGroup.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './styles/userRegister.css';
+import './styles/App.css';
 
 function CreateGroup() {
     const [groupName, setGroupName] = useState('');
